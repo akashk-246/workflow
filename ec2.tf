77332a2de8a3b94d2fd429f546a865b00c5df8f5
Akash K <akashk@phdata.io>
2026-07-25 23:54:42 +0530

    Format Terraform files

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 #1.vpc
 
 resource "aws_vpc" "vpc1" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "${var.customer}-vpc"
+    Name       = "${var.customer}-vpc"
     Managed_by = var.managed_by
   }
 }
@@ -15,7 +15,7 @@ resource "aws_internet_gateway" "igw1" {
   vpc_id = aws_vpc.vpc1.id
 
   tags = {
-    Name = "${var.customer}-igw1"
+    Name       = "${var.customer}-igw1"
     Managed_by = var.managed_by
   }
 }
@@ -26,7 +26,7 @@ resource "aws_subnet" "pub_sub1" {
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "${var.customer}-pub_sub1"
+    Name       = "${var.customer}-pub_sub1"
     Managed_by = var.managed_by
   }
 }
@@ -38,7 +38,7 @@ resource "aws_subnet" "pri_sub1" {
   cidr_block = "10.0.2.0/24"
 
   tags = {
-    Name = "${var.customer}-pri_sub1"
+    Name       = "${var.customer}-pri_sub1"
     Managed_by = var.managed_by
   }
 }
@@ -54,7 +54,7 @@ resource "aws_route_table" "pub_rt1" {
   }
 
   tags = {
-    Name = "${var.customer}-pub_rt1"
+    Name       = "${var.customer}-pub_rt1"
     Managed_by = var.managed_by
   }
 }
@@ -65,7 +65,7 @@ resource "aws_route_table" "pri_rt1" {
   vpc_id = aws_vpc.vpc1.id
 
   tags = {
-    Name = "${var.customer}-pri_rt1"
+    Name       = "${var.customer}-pri_rt1"
     Managed_by = var.managed_by
   }
 }
@@ -87,49 +87,49 @@ resource "aws_route_table_association" "prisubnet_rt1" {
 #9. Security Group1
 
 resource "aws_security_group" "sg1" {
-  name        = "${var.customer}-sg1"
+  name = "${var.customer}-sg1"
   #description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.vpc1.id
+  vpc_id = aws_vpc.vpc1.id
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["202.83.17.138/32", aws_vpc.vpc1.cidr_block]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["202.83.17.138/32", aws_vpc.vpc1.cidr_block]
   }
 
   ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "${var.customer}-sg1"
+    Name       = "${var.customer}-sg1"
     Managed_by = var.managed_by
   }
 }
 
 #10. EC2 web1
 resource "aws_instance" "web1" {
-  ami           = var.Ami_id
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.pub_sub1.id
-  key_name = "demo-key"
+  ami                         = var.Ami_id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.pub_sub1.id
+  key_name                    = "demo-key"
   associate_public_ip_address = "true"
-  vpc_security_group_ids = [aws_security_group.sg1.id]
+  vpc_security_group_ids      = [aws_security_group.sg1.id]
 
 
   tags = {
-    Name = "${var.customer}-web1"
+    Name       = "${var.customer}-web1"
     Managed_by = var.managed_by
   }
 }
--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
 
-variable "Ami_id"{
-    default = "ami-0b6d9d3d33ba97d99"
+variable "Ami_id" {
+  default = "ami-0b6d9d3d33ba97d99"
 }
 
-variable "customer"{
-    default = "coke"
+variable "customer" {
+  default = "coke"
 }
 
-variable "managed_by"{
-    default = "Terraform"
+variable "managed_by" {
+  default = "Terraform"
 }
